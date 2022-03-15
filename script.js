1a0864b9128cbd3b40597221919111f87d0bdbba
Thomas Aurelius <teg824@hotmail.com>
2022-03-15 14:24:07 -0500

    updated header

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=06698a6a31b873c971293972d78eab0d&page='
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=06698a6a31b873c971293972d78eab0d&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=06698a6a31b873c971293972d78eab0d&query="'
 
@@ -6,64 +6,63 @@ const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
-
+// Get initial movies
 getMovies(API_URL)
 
 async function getMovies(url) {
-   const res = await fetch(url)
-   const data = await res.json()
+    const res = await fetch(url)
+    const data = await res.json()
 
-   showMovies(data.results)
+    showMovies(data.results)
 }
 
 function showMovies(movies) {
-   main.innerHTML = ''
+    main.innerHTML = ''
 
-   movies.forEach((movie) => {
-      const { title, poster_path, vote_average, overview } = movie
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
 
-      const movieEl = document.createElement('div')
-      movieEl.classList.add('movie')
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
 
-      movieEl.innerHTML = `
-         <img src="${IMG_PATH + poster_path}" alt="${title}">
-         <div class="movie-info">
-            <h3>${title}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-         </div>
-         <div class="overview">
-            <h3>Overview</h3>
-            ${overview}
-         </div>
-         `
-      main.appendChild(movieEl)
-   })
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)
+    })
 }
+
 function getClassByRate(vote) {
-   if (vote >= 8) {
-      return 'green'
-   } else if (vote >=5) {
-      return 'orange'
-   } else {
-      return 'red'
-   }
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
-
 
 form.addEventListener('submit', (e) => {
-   e.preventDefault()
+    e.preventDefault()
 
-   const searchTerm = search.value
+    const searchTerm = search.value
 
-   if(searchTerm && searchTerm !== '') {
-      getMovies(SEARCH_API + searchTerm)
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
 
-      search.value = ''
-   } else {
-      window.location.reload()
-   }
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
 })
-
 const buttons = document.querySelectorAll('.btn')
 buttons.forEach((button,idx) => {
  
@@ -90,3 +89,6 @@ function highlightbuttons(idx){
     }
   })
 }
+
+
+
